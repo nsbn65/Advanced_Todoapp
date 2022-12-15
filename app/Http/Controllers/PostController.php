ef32ea6ccb51fed6fb7e1dd0bb61c72fb6b5da0d
nsbn65 <saito12pbyf8o@gmail.com>
2022-12-15 16:25:01 +0900

    advanced-todopj

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -49,42 +49,42 @@ class PostController extends Controller
 
     public function search(Request $request)
     {
-    $tag = new Tag;
-    $tags = $tag->getTags();
-    $keyword = $request->input('keyword');
-    $tag_name = $request->input('tag_name');
-    $user = Auth::user();
+        $tag = new Tag;
+        $tags = $tag->getTags();
+        $keyword = $request->input('keyword');
+        $tag_name = $request->input('tag_name');
+        $user = Auth::user();
 
     return view('search', 
     [
-    'tags' => [],
-    'keyword' => $keyword,
-    'tag_name' => $tag_name,
-    'user' => $user
+        'tags' => [],
+        'keyword' => $keyword,
+        'tag_name' => $tag_name,
+        'user' => $user
     ]);
     }
 
     public function find(Request $request)
     {
-    $keyword = $request->input('keyword'); 
-    $tag_name = $request->input('tag_name'); 
-    $user = Auth::user();
+        $keyword = $request->input('keyword'); 
+        $tag_name = $request->input('tag_name'); 
+        $user = Auth::user();
 
-    $query = Todo::query();
+        $query = Todo::query();
 
     if (isset($keyword)) 
     {
-    $query->where('content', 'like', '%' . self::escapeLike($keyword) . '%');
+        $query->where('content', 'like', '%' . self::escapeLike($keyword) . '%');
     }
     if (isset($tag_name)) 
     {
-    $query->where('tag_name', $tag_name);
+        $query->where('tag_name', $tag_name);
     }
 
-    $posts = $query->orderBy('tag_id', 'asc')->paginate(15);
+        $posts = $query->orderBy('tag_id', 'asc')->paginate(15);
 
-    $tag = new Tag;
-    $tags = $tag->getTags();
+        $tag = new Tag;
+        $tags = $tag->getTags();
 
     return view('search', [
         'posts' => $posts,
