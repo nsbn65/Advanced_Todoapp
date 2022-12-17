dc38b9ae0e533a11fa413181c4b00365e9734380
nsbn65 <saito12pbyf8o@gmail.com>
2022-12-17 20:27:10 +0900

    advanced-todopj

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-Route::group(['middleware' => ['auth']], function() {
-
-Route::get('/',[PostController::class,'index']);
-Route::post('/create',[PostController::class,'create'])->name('todo.create');
-Route::post('/update/{id}',[PostController::class,'update'])->name('todo.update');
-Route::post('/delete/{id}',[PostController::class,'delete'])->name('todo.delete');
-//Route::get('/', function () {})->middleware('auth');
-Route::post('/logout',[PostController::class,'login'])->name('todo.logout');
-Route::get('/search', [PostController::class,'search'])->name('todo.search');
-Route::post('/find', [PostController::class,'find'])->name('todo.find');
+Route::get('/',[PostController::class,'index'])->middleware('auth');
+Route::post('/create',[PostController::class,'create'])->name('todo.create')->middleware('auth');
+Route::post('/update/{id}',[PostController::class,'update'])->name('todo.update')->middleware('auth');
+Route::post('/delete/{id}',[PostController::class,'delete'])->name('todo.delete')->middleware('auth');
+Route::post('/logout',[PostController::class,'login'])->name('todo.logout')->middleware('auth');
+Route::get('/search', [PostController::class,'search'])->name('todo.search')->middleware('auth');
+Route::post('/find', [PostController::class,'find'])->name('todo.find')->middleware('auth');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
-});
 
 require __DIR__.'/auth.php';
